e77174b6d52cb1658e3a76667d151aa64a37d845
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-09-12 18:36:26 +0300

    task model added, accepted code status for api sync changed to acceptable codes

--- api/offer_api.py
+++ api/offer_api.py
@@ -3,19 +3,19 @@ from requests.auth import HTTPBasicAuth
 
 
 class OfferApiSYNC:
-    def __init__(self, auth: HTTPBasicAuth, url: str, data: list | dict, data_type: str = 'json', success_code: int = 200):
+    def __init__(self, auth: HTTPBasicAuth, url: str, data: list | dict, data_type: str = 'json', success_codes: int = 200):
         self.auth: HTTPBasicAuth = auth
         self.url: str = url
         self.data: list | dict = data
         self.data_type: str = data_type
-        self.success_code: int = success_code
+        self.success_codes: int = success_codes
         self.status: bool = False
         self.sync()
 
     def sync(self):
         payload: dict = {'auth': self.auth, 'url': self.url, 'json' if self.data_type == 'json' else 'data': self.data}
         response = api.post(**payload)
-        self.status = response.status_code == self.success_code
+        self.status = response.status_code in self.success_codes
 
     def __bool__(self):
         return self.status
--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
 from pathlib import Path
+
+from requests.auth import HTTPBasicAuth
+
 from models.api_model import ApiHostModel
 
 BASE_DIR: Path = Path(__file__).resolve().parent
@@ -11,3 +14,5 @@ OFFER_DATA_SOURCE = env('OFFER_DATA_SOURCE')
 API_HOST = ApiHostModel(env('API_HOST'))
 API_USER = env('API_USER')
 API_PASSWORD = env('API_PASSWORD')
+
+AUTH = HTTPBasicAuth(API_USER, API_PASSWORD)
